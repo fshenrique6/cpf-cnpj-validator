ced8e19ad7b5434d2edf87eb6e1e87e75bf614e4
Henrique Souza <henrique.fskt@gmail.com>
2026-09-21 14:52:33 -0300

    bugfix: fixed models/document.go and validator/document.go

--- backend/models/document.go
+++ backend/models/document.go
@@ -1,1 +1,12 @@
 package models
+
+import "time"
+
+type Document struct {
+	ID          int       `json:"id"`
+	Number      string    `json:"number" binding:"required"`
+	Type        string    `json:"type"`
+	Blocklisted bool      `json:"blocklisted"`
+	CreatedAt   time.Time `json:"created_at"`
+	UpdatedAt   time.Time `json:"updated_at"`
+}

952fd44520b53d2a041cb4cebff743a9450d9eff
Henrique Souza <henrique.fskt@gmail.com>
2026-09-21 10:42:41 -0300

    feat: added database.go & main.go

--- backend/database/database.go
+++ backend/database/database.go
@@ -1,1 +1,40 @@
 package database
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	_ "github.com/jackc/pgx/v5/stdlib"
+)
+
+var DB *sql.DB
+
+func Connect() {
+	if err := godotenv.Load(); err != nil {
+		log.Println("aviso: arquivo .env não encontrado, usando variáveis de ambiente do sistema")
+	}
+
+	connStr := fmt.Sprintf(
+		"postgres://%s:%s@%s:%s/%s?sslmode=disable",
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_PORT"),
+		os.Getenv("DB_NAME"),
+	)
+
+	db, err := sql.Open("pgx", connStr)
+	if err != nil {
+		log.Fatal("erro ao conectar no banco:", err)
+	}
+
+	if err := db.Ping(); err != nil {
+		log.Fatal("erro ao pingar o banco:", err)
+	}
+
+	DB = db
+	log.Println("conectado ao banco com sucesso")
+}
---
+++ backend/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"cpf-cnpj-validator/database"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	database.Connect()
+
+	router := gin.Default()
+
+	router.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{"message": "pong"})
+	})
+
+	router.Run(":8080")
+}
